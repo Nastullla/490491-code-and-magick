4393664843d2f158b4e68fd53983d706f2f5c39a
Nastullla <Nastullla@mail.ru>
2018-11-23 10:27:32 +0300

    Исправлены названия массивов и функций

--- js/setup.js
+++ js/setup.js
@@ -1,26 +1,26 @@
 'use strict';
 
+var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var WIZARD_COATCOLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var WIZARD_EYESCOLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+
 var setupWindow = document.querySelector('.setup');
 setupWindow.classList.remove('hidden');
-
-var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-var WIZARD_SURNAME = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var WIZARD_COATCOLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-var WIZARD_EYESCOLOR = ['black', 'red', 'blue', 'yellow', 'green'];
 
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 var setupSimilar = document.querySelector('.setup-similar');
 var similarListElement = setupSimilar.querySelector('.setup-similar-list');
 
-var randomSelection = function (array) {
+var selectRandomItem = function (array) {
   return array[Math.floor(Math.random() * array.length)];
 };
 
 var createWizard = function () {
   return {
-    name: randomSelection(WIZARD_NAMES) + ' ' + randomSelection(WIZARD_SURNAME),
-    coatColor: randomSelection(WIZARD_COATCOLOR),
-    eyesColor: randomSelection(WIZARD_EYESCOLOR)
+    name: selectRandomItem(WIZARD_NAMES) + ' ' + selectRandomItem(WIZARD_SURNAMES),
+    coatColor: selectRandomItem(WIZARD_COATCOLORS),
+    eyesColor: selectRandomItem(WIZARD_EYESCOLORS)
   };
 };
 
